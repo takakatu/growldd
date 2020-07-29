f4f7906b62dc733e0238f2f77fd30f40df697c86
takakatu <takakatu0201@gmail.com>
2020-07-30 08:27:05 +0900

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root to: 'posts#index'
+  resources :posts, only: [:show, :index, :new, :create, :edit, :destroy]
+  resources :users, only: [:index, :edit]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
